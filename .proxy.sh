4ad849fec257a63de5b2e8da8fdd0d460f174443
xiaomimila <51012615+xiaomimila@users.noreply.github.com>
2022-12-05 23:55:12 +0800

    Create .proxy.sh

---
+++ .proxy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+proxy=$(ps -ef | grep -w prox[y] | wc -l)
+if [[ $proxy = 0 ]]
+then
+  echo "Start..."
+  vpn_ip=$(vpn ip)
+  sleep 2s
+  /usr/bin/proxy http -t tcp -p $vpn_ip:80,$vpn_ip:443 --dns-address "1.1.1.1:53" --dns-ttl 300 --daemon --forever
+else
+	echo "already started"
+fi
