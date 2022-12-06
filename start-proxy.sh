6a5d9c9a144d59c0eefcf441d8c5c0f5750ea3e5
xiaomimila <51012615+xiaomimila@users.noreply.github.com>
2022-12-06 11:07:23 +0800

    Update start-proxy.sh

--- start-proxy.sh
+++ start-proxy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+
 proxy=$(ps -ef | grep -w prox[y] | wc -l)
 if [[ $proxy = 0 ]]
 then
@@ -9,5 +10,27 @@ then
   /usr/bin/proxy http -t tcp -p $vpn_ip:80,$vpn_ip:443 --dns-address "1.1.1.1:53" --dns-ttl 300 --daemon --forever
 else
   echo "already started"
+fi
+
+
+ipv6=$(/sbin/ip6tables -L)
+if [[ $ipv6 =~ DROP ]]
+then
+  echo "Yes"
+else
+	echo "No"
+   ipt6="/sbin/ip6tables"
+   $ipt6 -F
+   $ipt6 -X
+   $ipt6 -Z
+   $ipt6 -P INPUT DROP
+   $ipt6 -P FORWARD DROP
+   $ipt6 -P OUTPUT ACCEPT
+   $ipt6 -A INPUT -i lo -j ACCEPT
+   $ipt6 -A INPUT -p tcp --syn -j DROP
+   $ipt6 -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+   $ipt6 -A INPUT -p ipv6-icmp -j ACCEPT
+   $ipt6 -A INPUT -m state --state NEW -m udp -p udp -s fe80::/10 --dport 546 -j ACCEPT
+   service ip6tables save && systemctl restart ip6tables
 fi
 
